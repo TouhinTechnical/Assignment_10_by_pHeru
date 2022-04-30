b7c5cd0df331dff7167540bfc19a3f1a867e371e
TouhinTechnical <touhidulislam1090@gmail.com>
2022-04-30 19:23:57 +0600

    Footer created

--- cyber-security/src/Pages/Shared/Footer/Footer.js
+++ cyber-security/src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h3 className='text-center'>This is footer</h3>
-        </div>
+        <footer className='footerSection'>
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-6">
+                        <div className="copyright">
+                            <span>Copyright @ 2022 Cyber Security Design by <Link to="" target="_blank">TouhinTechnical</Link></span>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="privacyPart">
+                            <span className='me-5 pri1'>Privacy Policy</span>
+                            <span className='pri2'>Terms & Conditions</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
